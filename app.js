c80bbe7b7cd9a7934fdf180a09741f170378b8c9
Zeydi <oussema.zeidi@ieee.org>
2017-12-11 21:18:40 +0100

    created server of the app

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+
+
+app;use("/", (req, res) =>{
+  res.send("HELLO WORLD")
+});
+
+app.listen(port, () => {
+  console.log("Server Listening on port" + port);
+});
