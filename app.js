198bc6229049f74c32c5545206a5a2d09c0cde9d
Zeydi <oussema.zeidi@ieee.org>
2017-12-11 21:45:46 +0100

    added database mongodb

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 var express = require('express');
 var app = express();
 var port = 3000;
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/node-app");
 
+var nameSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String
+});
+
+var User = mongoose.model("User, nameSchema")
 
 app.use("/", (req, res) =>{
   res.sendFile(__dirname + "/index.html");
