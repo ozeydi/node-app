583329b8e7c630f50e68d4fec139e0e9f34e2830
Zeydi <oussema.zeidi@ieee.org>
2017-12-11 21:59:29 +0100

    added middleware body-parser

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 var port = 3000;
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost:27017/node-app");
